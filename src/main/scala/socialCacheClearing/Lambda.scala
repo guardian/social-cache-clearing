0dcf792ef309e6cbd88ab678b71d3d911e1b6ac4
amyhughes <amy.hughes@guardian.co.uk>
2019-02-21 14:42:14 +0000

    add processor to return true for update events

--- src/main/scala/socialCacheClearing/Lambda.scala
+++ src/main/scala/socialCacheClearing/Lambda.scala
@@ -1,11 +1,10 @@
 package socialCacheClearing
 
-import com.amazonaws.services.kinesis.clientlibrary.types.ProcessRecordsInput
 import com.amazonaws.services.lambda.runtime.events.KinesisEvent
 import com.amazonaws.services.kinesis.model.Record
 import scala.collection.JavaConverters._
 import com.amazonaws.services.kinesis.clientlibrary.types.UserRecord
-
+import com.gu.crier.model.event.v1.EventType
 
 object Lambda {
   def handler(event: KinesisEvent): Unit = {
@@ -16,20 +15,17 @@ object Lambda {
     println(s"Processing ${userRecords.size} records ...")
 
     RecordProcessor.process(userRecords.asScala) { event =>
-      (event.itemType, event.eventType) match {
-        case (ItemType.Content, EventType.Delete) =>
-          sendFastlyPurgeRequestAndAmpPingRequest(event.payloadId, Hard)
-        case (ItemType.Content, EventType.Update) =>
-          sendFastlyPurgeRequest(event.payloadId, Soft)
-        case (ItemType.Content, EventType.RetrievableUpdate) =>
-          sendFastlyPurgeRequest(event.payloadId, Soft)
+      event.eventType match {
+        case EventType.Update => sendTwitterDecacheRequest(event.payloadId)
+        case EventType.RetrievableUpdate => sendTwitterDecacheRequest(event.payloadId)
+        case other => false
+      }
 
-        case other =>
-          // for now we only send purges for content, so ignore any other events
-          false
+      def sendTwitterDecacheRequest(id: String): Boolean = {
+        println(s"sending Twitter decache request for ${id}")
+        true
       }
     }
 
-    ???
   }
 }
