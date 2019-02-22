2d4bbb3dcd261a4d33941b54b8e71e7ebe588220
amyhughes <amy.hughes@guardian.co.uk>
2019-02-22 17:54:06 +0000

    remove seperate record processor

--- src/main/scala/socialCacheClearing/Lambda.scala
+++ src/main/scala/socialCacheClearing/Lambda.scala
@@ -1,31 +1,36 @@
 package socialCacheClearing
 
-import com.amazonaws.services.lambda.runtime.events.KinesisEvent
-import com.amazonaws.services.kinesis.model.Record
-import scala.collection.JavaConverters._
 import com.amazonaws.services.kinesis.clientlibrary.types.UserRecord
-import com.gu.crier.model.event.v1.EventType
+import com.amazonaws.services.kinesis.model.Record
+import com.amazonaws.services.lambda.runtime.events.KinesisEvent
+import com.gu.crier.model.event.v1.EventType._
+import com.gu.crier.model.event.v1.{Event, EventType}
+import com.gu.thrift.serializer.ThriftDeserializer
+
+import scala.collection.JavaConverters._
 
 class Lambda {
+  val updateEventTypes = Set[EventType](Update, RetrievableUpdate)
+
   def handler(event: KinesisEvent): Unit = {
 
     val rawRecords: List[Record] = event.getRecords.asScala.map(_.getKinesis).toList
-    val userRecords = UserRecord.deaggregate(rawRecords.asJava)
+    val userRecords = UserRecord.deaggregate(rawRecords.asJava).asScala
 
     println(s"Processing ${userRecords.size} records ...")
 
-    RecordProcessor.process(userRecords.asScala) { event =>
-      event.eventType match {
-        case EventType.Update => sendTwitterDecacheRequest(event.payloadId)
-        case EventType.RetrievableUpdate => sendTwitterDecacheRequest(event.payloadId)
-        case other => false
-      }
-    }
+    val idsForUpdateEvents: Set[String] = updateEventIds(userRecords)
 
+    idsForUpdateEvents.foreach(sendTwitterDecacheRequest)
   }
 
-  def sendTwitterDecacheRequest(id: String): Boolean = {
+  def sendTwitterDecacheRequest(id: String) = {
     println(s"sending Twitter decache request for ${id}")
-    true
+  }
+
+  def updateEventIds(records: Seq[UserRecord]): Set[String] = {
+    records.flatMap { record =>
+      ThriftDeserializer.deserialize(record.getData.array)(Event).toOption
+    }.filter(event => updateEventTypes.contains(event.eventType)).map(_.payloadId).toSet
   }
 }
